a7102fd198e8359ca36f302809af8e8e48820b31
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-06-06 08:03:26 +0530

    authentication routes and controllers setup: register and login

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 import app from './src/app.js';
+import connectDB from './src/config/database.js';
 dotenv.config();
 
+
+await connectDB();
 let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,2 @@
+export const registerController = async (req, res) => {}
+export const loginController = async (req, res) => {}
