668e151d3e022f0bac2d493cfee8a98f392e27ee
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-06-06 08:57:29 +0530

    feat: add login controller with jwt authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -48,9 +48,62 @@ export const registerController = async (req, res) => {
     });
   } catch (error) {
     return res.status(500).json({
-      message: "Server error",
+      message: "Internal server error",
       error: error.message,
     });
   }
 };
-export const loginController = async (req, res) => {};
+export const loginController = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    // Basic validation
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const isUserExist = await UserModel.findOne({ email });
+
+    if (!isUserExist) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const comparePassword = await bcrypt.compare(
+      password,
+      isUserExist.password,
+    );
+
+    if (!comparePassword) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    let token = jwt.sign({ id: isUserExist._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+    });
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        id: isUserExist._id,
+        username: isUserExist.username,
+        email: isUserExist.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
