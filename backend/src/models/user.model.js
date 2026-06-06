ade75e052d4fae09ee46e21221001b7941715b56
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-06-06 08:18:13 +0530

    feat: add user schema with authentication fields

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email must be unique"],
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+  },
+  avatar: {
+    type: String,
+    default: "",
+  },
+  lastSeen: {
+    type: Date,
+    default: Date.now,
+  },
+},{
+    timestamps: true,
+})
+
+const UserModel = mongoose.model("User", userSchema);
+export default UserModel;
